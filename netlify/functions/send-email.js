94065da1924117caf04802eb48a74c6794c41e46
kfirkfir89 <kfirkfir89@gmail.com>
2023-06-26 02:00:12 +0300

    fix try for netlify

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,30 +1,28 @@
-/* eslint-disable object-shorthand */
-// eslint-disable-next-line import/no-unresolved, @typescript-eslint/no-var-requires
-const sendgrid = require('@sendgrid/mail');
+// const sendgrid = require('@sendgrid/mail');
 
-exports.handler = async (event, context) => {
-  const { SENDGRID_API_KEY } = process.env;
-  sendgrid.setApiKey(SENDGRID_API_KEY);
+// exports.handler = async (event, context) => {
+//   const { SENDGRID_API_KEY } = process.env;
+//   sendgrid.setApiKey(SENDGRID_API_KEY);
 
-  const { email, subject, message } = JSON.parse(event.body);
+//   const { email, subject, message } = JSON.parse(event.body);
 
-  const emailData = {
-    to: email,
-    from: 'your-email@yourdomain.com', // replace this with your own email
-    subject: subject,
-    text: message,
-  };
+//   const emailData = {
+//     to: email,
+//     from: 'your-email@yourdomain.com', // replace this with your own email
+//     subject: subject,
+//     text: message,
+//   };
 
-  try {
-    await sendgrid.send(emailData);
-    return {
-      statusCode: 200,
-      body: 'Email sent',
-    };
-  } catch (error) {
-    return {
-      statusCode: error.code,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
-};
+//   try {
+//     await sendgrid.send(emailData);
+//     return {
+//       statusCode: 200,
+//       body: 'Email sent',
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: error.code,
+//       body: JSON.stringify({ error: error.message }),
+//     };
+//   }
+// };
